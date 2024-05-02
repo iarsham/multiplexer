0242e5c14ac6e00e1338869c702a31ba9352e7ab
Arsham Roshannejad <arshamdev2001@gmail.com>
2024-05-02 20:45:08 +0330

    add:Enhanced route registration and handling in Router for flexible pattern matching and request handling.

--- mux.go
+++ mux.go
@@ -1,6 +1,12 @@
 package multiplexer
 
-import "net/http"
+import (
+	"net/http"
+	"regexp"
+)
+
+// reGo122 is a compiled regular expression used to match and extract two groups from a string pattern.
+var reGo122 = regexp.MustCompile(`^(\S*)\s+(.*)$`)
 
 // Middlewares represents a list of middleware functions.
 type Middlewares []func(http.Handler) http.Handler
@@ -25,4 +31,27 @@ func New(mux *http.ServeMux) *Router {
 // It calls the ServeHTTP method of the underlying http.ServeMux.
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	r.mux.ServeHTTP(w, req)
+}
+
+// Registers a route with the router, prepending base path for named capture groups.
+// pattern (string): URL pattern with optional named capture groups (e.g., `/users/:id`).
+// handler (http.HandlerFunc): Function to handle requests matching the pattern.
+func (r *Router) register(pattern string, handler http.HandlerFunc) {
+	match := reGo122.FindStringSubmatch(pattern)
+	if len(match) > 2 {
+		pattern = match[1] + " " + r.path + match[2]
+	} else {
+		pattern = r.path + pattern
+	}
+	r.mux.HandleFunc(pattern, handler)
+}
+
+// HandleFunc adds a new route with the given pattern and handler function.
+func (r *Router) HandleFunc(pattern string, handler http.HandlerFunc) {
+	r.register(pattern, handler)
+}
+
+// Handle adds a new route with the given pattern and handler.
+func (r *Router) Handle(pattern string, handler http.Handler) {
+	r.register(pattern, handler.ServeHTTP)
 }
