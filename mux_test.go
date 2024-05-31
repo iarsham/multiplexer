c650fd5f98448028033823da6683673c89b67bdd
Arsham Roshannejad <arshamdev2001@gmail.com>
2024-06-01 03:18:47 +0330

    add:handling htpp 404 & 405 tests

---
+++ mux_test.go
@@ -0,0 +1,59 @@
+package multiplexer
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHttpNotFound(t *testing.T) {
+	mux := New(http.NewServeMux(), "/")
+	mux.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("custom not found handler"))
+	})
+	ts := httptest.NewServer(mux)
+	defer ts.Close()
+	resp, err := http.Get(ts.URL + "/api/blah-blah")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(body) != "custom not found handler" {
+		t.Fatalf("expected: custom not found handler, got %s", string(body))
+	}
+}
+
+func TestHttpMethodNotAllowed(t *testing.T) {
+	mux := New(http.NewServeMux(), "/")
+	mux.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("custom method not allowed handler"))
+	})
+	mux.HandleFunc("GET api/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello"))
+	})
+	ts := httptest.NewServer(mux)
+	defer ts.Close()
+	resp, err := http.Post(ts.URL+"/api/hello", "application/json", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(body) == "hello" {
+		t.Fatalf("expected: custom method not allowed handler, got %s", string(body))
+	}
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Fatalf("expected: %d, got %d", http.StatusMethodNotAllowed, resp.StatusCode)
+	}
+}
